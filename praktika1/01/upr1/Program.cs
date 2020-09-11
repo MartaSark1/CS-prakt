280a4c82322c9fe4f6d81e606c4885ae64707a84
Marta <student@C251U30>
2020-09-11 13:20:05 +0300

    начало

---
+++ praktika1/01/upr1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace upr1
+{
+  class Program
+  {
+    struct Person
+    {
+    }
+
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
